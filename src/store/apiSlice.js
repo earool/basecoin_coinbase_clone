aea3c0ea3d0013826a6eb853893ebb85490fa7b2
earool <dawid.baranski0000@gmail.com>
2023-05-04 10:36:07 +0200

    feat: implement function for generating home data query URL

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -1,13 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-import createWatchlistFetchUrl from '../utils/createWatchlistFetchUrl';
+import createHomeFetchUrl from '../utils/createHomeFetchUrl';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.coingecko.com/api/v3' }),
   endpoints: (builder) => ({
     getHomeData: builder.query({
-      query: (coinsIds) => createWatchlistFetchUrl(coinsIds),
+      query: ({ watchlistIds: coinsIds, option }) =>
+        createHomeFetchUrl(option, coinsIds),
     }),
   }),
 });
---
+++ src/utils/createHomeFetchUrl.js
@@ -0,0 +1,16 @@
+export default function createHomeFetchUrl(option, coinsIds) {
+  if (option === 'Watchlist') {
+    const string = coinsIds.join('%2C');
+    return `/coins/markets?vs_currency=usd&ids=${string}&order=market_cap_desc&page=1&sparkline=true&price_change_percentage=24h&locale=en`;
+  }
+
+  if (option === 'Top assets') {
+    return 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&price_change_percentage=24h&locale=en';
+  }
+
+  if (option === 'Trending') {
+    return 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=volume_desc&per_page=10&page=1&sparkline=true&price_change_percentage=24h&locale=en';
+  }
+
+  return null;
+}
