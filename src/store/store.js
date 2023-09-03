6e1164cbeb2353293a0a26cdfc346db9e74742d1
earool <dawid.baranski0000@gmail.com>
2023-09-03 19:33:33 +0200

    feat: replace coingecko api with coinranking api
    To prevent exceeding the rate limits on the CoinGecko API,
    we are changing the data source to Coinraking API.
    This change ensures reliable access to the required data
    without encountering rate limit issues.

--- src/store/store.js
+++ src/store/store.js
@@ -2,14 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from './authSlice';
 import userSliceReducer from './userSlice';
-import { apiSlice } from './apiSlice';
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     user: userSliceReducer,
-    [apiSlice.reducerPath]: apiSlice.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
 });
