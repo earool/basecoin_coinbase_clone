49aaec0c8f90dc16d738f6fd7433db3e65aa4c64
earool <dawid.baranski0000@gmail.com>
2023-11-30 15:48:40 +0100

    fix: ensure if node is valid before observing it

--- src/hooks/useOnScreen.jsx
+++ src/hooks/useOnScreen.jsx
@@ -3,17 +3,14 @@ import React, { useRef, useCallback } from 'react';
 function useOnScreen(intersectionCallback, styleClass) {
   const observer = useRef();
 
-  console.log(observer);
-
   const ref = useCallback(
     (node) => {
       if (observer.current) observer.current.disconnect();
 
       if (node) {
         observer.current = new IntersectionObserver(intersectionCallback);
+        observer.current.observe(node);
       }
-
-      observer.current.observe(node);
     },
     [intersectionCallback]
   );
