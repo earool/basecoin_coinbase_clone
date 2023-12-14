d892f86d323a512ede638c1850f85aa127b4d216
earool <dawid.baranski0000@gmail.com>
2023-12-14 21:00:22 +0100

    feat: make an input scalable

--- src/components/protected/buysell_container/FlexibleInput.jsx
+++ src/components/protected/buysell_container/FlexibleInput.jsx
@@ -1,45 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+
+const SCALE_STEP = 0.05;
+
+function calcMaxScale(containerWidth, textWidth, scale) {
+  if (containerWidth < textWidth * scale) {
+    return scale > 1 ? 1 : scale - SCALE_STEP;
+  }
+  return calcMaxScale(containerWidth, textWidth, scale + SCALE_STEP);
+}
+
+function calcScale(containerWidth, textWidth, scale) {
+  if (containerWidth > textWidth * scale) {
+    if (scale >= 1) {
+      return 1;
+    }
+    return calcMaxScale(containerWidth, textWidth, scale);
+  }
+  return calcScale(containerWidth, textWidth, scale - SCALE_STEP);
+}
 
 function FlexibleInput() {
-  // eslint-disable-next-line no-unused-vars
   const [styles, setStyles] = useState({
-    dMarginLeft: '160px',
-    iFontSize: '64px',
+    iWidth: '36px',
+    scale: 1,
   });
+  const measuringTextDiv = useRef();
 
+  // make a validation of the input
   const changeHandler = (e) => {
-    const length = e.target.value.length > 1 ? e.target.value.length : 1;
-    const width = e.target.clientWidth;
-    console.log(width);
+    const containerWidth = e.target.parentElement.parentElement.clientWidth;
+    measuringTextDiv.current.innerText = e.target.value || '0';
+    const textWidth = measuringTextDiv.current.clientWidth;
 
     setStyles((prevs) => {
-      const { dMarginLeft, iFontSize } = prevs;
-      let ml = parseInt(dMarginLeft, 10);
-      let fs = parseInt(iFontSize, 10);
-
-      const isOverflowing = ml < 40;
-      ml = isOverflowing ? ml : 160 - 30 * (length - 1);
-      // doesnt work right now
-      if (isOverflowing && width > 264) {
-        fs *= 0.7;
-      }
-
-      return { dMarginLeft: `${ml}px`, iFontSize: `${fs}px` };
+      const newScale = calcScale(containerWidth, textWidth, prevs.scale);
+      return {
+        iWidth: textWidth,
+        scale: newScale,
+      };
     });
   };
 
   return (
-    <div className="flex ml-[160px]" style={{ marginLeft: styles.dMarginLeft }}>
-      <div className="text-[25px] text-gray-border-darker w-[14px]">
-        <span>$</span>
+    <div
+      className="flex justify-center max-w-[232px] flex-1"
+      style={{ transform: `scale(${styles.scale})` }}
+    >
+      <div className="flex justify-center">
+        <span className="text-[25px] text-gray-border-darker w-[14px]">$</span>
+        <input
+          inputMode="decimal"
+          className="focus:outline-none text-my-blue text-[64px] box-border overflow-hidden"
+          onChange={changeHandler}
+          placeholder="0"
+          minLength={1}
+          style={{ width: styles.iWidth }}
+        />
+        <div
+          ref={measuringTextDiv}
+          className="invisible w-auto fixed min-width-[36px] text-[64px]"
+        />
       </div>
-      <input
-        className="focus:outline-none text-my-blue text-[64px] w-full"
-        onChange={changeHandler}
-        placeholder="0"
-        minLength={1}
-        style={{ fontSize: styles.iFontSize }}
-      />
     </div>
   );
 }
--- src/components/protected/buysell_container/TransactionInputContainer.jsx
+++ src/components/protected/buysell_container/TransactionInputContainer.jsx
@@ -5,9 +5,9 @@ import { ReactComponent as ArrowsIcon } from '../../../assets/icons/others/veric
 
 function TransactionInputContainer() {
   return (
-    <div className="py-10 flex justify-end">
+    <div className="py-10 relative flex justify-center">
       <FlexibleInput />
-      <div className="flex flex-col justify-center min-w-[72px]">
+      <div className="flex flex-col justify-center min-w-[64px] absolute right-0 bottom-[calc(50%-26px)]">
         <div className="w-8 h-8 border-0 rounded-[14px] bg-gray-light flex justify-center items-center">
           <ArrowsIcon className="w-1/2 h-1/2" />
         </div>
